20cd8fbb3a4374e9f539bc358cae7ceb13eabcd9
Lucas Chapin <lchapin@wanderu.com>
2020-04-25 16:48:08 -0400

    refactor with decorator

--- dash/sql.py
+++ dash/sql.py
@@ -4,18 +4,21 @@ import pandas as pd
 
 from connection import css_connection
 
-def query_to_df(sql):
-    """Helper to run sql and return dataframe"""
-    with css_connection() as conn:
-        return pd.read_sql_query(sql, conn)
 
+def query_to_df(sql_func):
+    """Helper to run sql and return dataframe"""
+    def get_df():
+        sql = sql_func()
+        with css_connection() as conn:
+            return pd.read_sql_query(sql, conn)
+    return get_df
+
+@query_to_df
 def orders_by_status():
-    """Query SQLite, Return dataframe"""
-    sql = """
+    return """
     SELECT status
     , COUNT(*) as cnt
     FROM orders
     GROUP BY status
     ORDER BY status;
     """
-    return query_to_df(sql)
